70799b7da2a3a06ea3e04eaaf21ab79952ceb708
Sebastián Varela <sebastian.varela@s3ba.net>
2017-01-17 21:00:48 +0100

    Add empty project

---
+++ ToastController/ToastController.h
@@ -0,0 +1,19 @@
+//
+//  ToastController.h
+//  ToastController
+//
+//  Created by Sebastián Varela on 17/1/17.
+//  Copyright © 2017 CIBER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToastController.
+FOUNDATION_EXPORT double ToastControllerVersionNumber;
+
+//! Project version string for ToastController.
+FOUNDATION_EXPORT const unsigned char ToastControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastController/PublicHeader.h>
+
+
